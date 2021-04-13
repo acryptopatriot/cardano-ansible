f257a3c200972c69714d780293fc138f53a1e041
acryptopatriot <dev@cryptopatriot.io>
2021-04-13 11:01:14 -0500

    Tested and validated release. Builds multi-node setup.

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-sudo apt update
-sudo apt install ansible
-sudo apt install sshpass
+echo "Updating apt"
+#sudo apt update
+
+echo "Installing ansible"
+#sudo apt install ansible
+
+echo "Installing sshpass"
+#sudo apt install sshpass
+
+# Generate ssh keys
+echo "Please enter passphrase for ssh key"
+read sshKeyPasscode
+
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/cardano_rsa -P $sshKeyPasscode
+chmod 400 ~/.ssh/cardano
+
+echo $sshKeyPasscode
